bb6c567ff9bcc07a92ccb9d3e223004e35a36786
one4sv <kutenko18@gmail.com>
2025-10-13 23:52:23 +0300

    в

--- confirm.js
+++ confirm.js
@@ -94,6 +94,11 @@ export default function (app, supabase) {
       await supabase.from("pending_users").delete().eq("id", pendingUser.id);
 
       // JWT
+      res.setHeader("Access-Control-Allow-Origin", process.env.CLIENT_URL);
+      res.setHeader("Access-Control-Allow-Credentials", "true");
+      res.cookie("token", jwtToken, { httpOnly: true, secure: true, sameSite: "none", maxAge: 30*24*60*60*1000 });
+      console.log("Set-Cookie header:", res.getHeader("Set-Cookie"));
+
       const jwtToken = jwt.sign({ id: newUser.id }, SECRET, { expiresIn: "30d" });
       res.cookie("token", jwtToken, { httpOnly: true, secure: true, sameSite: "none", maxAge: 30*24*60*60*1000 });
       console.log(res.getHeader("Set-Cookie"));
